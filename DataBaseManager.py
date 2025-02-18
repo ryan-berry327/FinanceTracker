a63ca5a3b1d2b58bf2855b11e120295680efe59e
ryan-berry327 <ryanberry480@gmail.com>
2025-02-18 10:51:06 +0000

    Set up the database and added code to other functions

--- DataBaseManager.py
+++ DataBaseManager.py
@@ -1,5 +1,34 @@
+import sqlite3
+
 class DatabaseManager:
-    connection = '' # connect to the database
+
+    # Initialise the database connection
+    def __init__(self, db_name = "FinanceTracker.db"):
+        self.conn = sqlite3.connect(db_name) #Can set up different db in the future if want to by removing the given db name
+        self.cursor = self.conn.cursor()
+        self.create_tables() # automatically create tables when the class is initialised
+
+
+    def create_tables(self):
+
+        # If table doesnt exist create it
+        self.cursor.execute("""
+        CREATE TABLE IF NOT EXISTS Transactions (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            amount REAL NOT NULL
+            category TEXT NOT NULL,
+            date TEXT NOT NULL CHECK(date LIKE '____-__-__'), # Ensures YYYY-MM-DD format
+            type TEXT CHECK(type IN ('income','expense')) NOT NULL,
+            description TEXT DEFAULT '',
+            timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+        )
+        """)
+
+        self.conn.commit() # save changes
+
+    # Close the DB connection
+    def close_connection(self):
+        self.conn.close()
 
     def initialise_database():
         pass
