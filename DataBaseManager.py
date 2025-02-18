ecefd001846ed140d0f2232901c05caf45316c70
ryan-berry327 <ryanberry480@gmail.com>
2025-02-18 11:18:22 +0000

    Finished database (I think) going to test it

--- DataBaseManager.py
+++ DataBaseManager.py
@@ -24,17 +24,37 @@ class DatabaseManager:
         """)
         self.conn.commit()  # Save changes
 
-    def insert_transaction(self,transaction):
-        pass
+    # Insert a transaction into the database
+    def insert_transaction(self,amount,category,date,type,description = ""):
+        self.cursor.execute("""
+        INSERT INTO transactions (amount, category, date, type, description)
+        VALUES (?, ?, ?, ?, ?)
+        """, (amount,category,date,type,description))
+        self.conn.commit()
     
-    def fetch_transaction():
-        pass
+    # Gets transaction
+    def fetch_transaction(self,filter_by = None, value = None):
+        if filter_by == 'category':
+            self.cursor.execute("SELECT * FROM transactions WHERE category = ?", (value,))
+        elif filter_by == 'date':
+            self.cursor.execute("SELECT * FROM transactions WHERE date = ?", (value,))
+        else:
+            self.cursor.execute("SELECT * FROM transactions")
+        return self.cursor.fetchall()
 
-    def delete_transaction(transaction_id):
-        pass
+    # Delete a transaction by its ID
+    def delete_transaction(self,transaction_id):
+        self.cursor.execute("DELETE FROM transactions WHERE ID =?", (transaction_id))
+        self.conn.commit()
 
+    # Calculates the balance (total income - total expense)
     def get_balance(self):
-        pass
+        self.cursor.execute("SELECT SUM(amount) FROM transactions WHERE type = 'income'")
+        income = self.cursor.fetchone()[0] or 0 # default to 0 if None
+        self.cursor.execute("SELECT SUM(amount) FROM transactions WHERE type = 'expense'")
+        expense = self.cursor.fetchone()[0] or 0 # default to 0 if None
+        return income - expense
+
     # Close the DB connection
     def close_connection(self):
         self.conn.close()
